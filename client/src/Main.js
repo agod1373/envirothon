b3b7dfece4abcbaec7d9763100fbf3867a18a0cc
Alex Godfrey <alexjamesgodfrey@gmail.com>
2021-05-10 19:56:13 -0400

    basic question upload

--- client/src/Main.js
+++ client/src/Main.js
@@ -3,11 +3,13 @@ import { useAuth } from './contexts/AuthContext'
 import { Nav, Navbar } from 'react-bootstrap';
 import Authentication from './components/authentication/Authentication'
 import Events from './components/events/Events'
+import Upload from './components/upload/Upload.js'
 
 export default function Main() {
     const [bg, setBG] = useState('white')
     const [text, setText] = useState('#7269bf')
     const [light, setLight] = useState('dark')
+    const [upload, setUpload] = useState(false)
 
     const lightMode = () => {
         if (light === 'light') {
@@ -24,14 +26,20 @@ export default function Main() {
     return (
         <div className="main" style={{backgroundColor: bg}}>
             <Navbar bg="success" id="head">
-                <Navbar.Brand style={{ marginLeft: '10px', color: bg }}>envirothon.study</Navbar.Brand>
-                <Nav.Link style={{ color: bg}}>upload a question</Nav.Link>
+                <Navbar.Brand onClick={() => setUpload(false)} style={{ marginLeft: '10px', color: bg }}>envirothon.study</Navbar.Brand>
+                <Nav.Link onClick={() => setUpload(true)} style={{ color: bg}}>upload a question</Nav.Link>
                 <Navbar.Collapse className="justify-content-end">
                 <Navbar.Text style={{marginRight: '10px'}} onClick={lightMode}>{light}</Navbar.Text>
             </Navbar.Collapse>
             </Navbar>
-            <Authentication text={text} />
-            <Events text={text} />
+            {!upload ?
+                <div>
+                    <Authentication text={text} />
+                    <Events text={text} />
+                </div>
+                :
+                <Upload />
+            }
         </div>
     )
 }
---
+++ client/src/components/upload/Upload.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+export default function Upload() {
+    return (
+        <div>
+            question upload coming soon
+        </div>
+    )
+}
