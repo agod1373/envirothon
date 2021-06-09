ce6b741cde4da92ee410c6760b6344ac233caad3
Alex Godfrey <alexjamesgodfrey@gmail.com>
2021-06-09 07:39:27 -0400

    basic question answerign with db

--- client/src/components/events/QuestionReal.js
+++ client/src/components/events/QuestionReal.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useTheme } from '../../contexts/ThemeContext'
+import { useAuth } from '../../contexts/AuthContext'
 import ReactPlayer from 'react-player'
 import ReactAudioPlayer from 'react-audio-player'
 import { Card, Button, Form } from 'react-bootstrap'
@@ -8,11 +9,24 @@ import './Question.scss'
 
 export default function QuestionReal(props) {
     const { themeBackground, themeText, themeVariant } = useTheme()
+    const { currentUser } = useAuth()
     const [answers, setAnswers] = useState(props.q.answers.split(','))
     const [answerText, setAnswerText] = useState()
     const [selected, setSelected] = useState()
     const [answered, setAnswered] = useState(false)
     const [checkmark, setCheckmark] = useState(false)
+    const [answerLog, setAnswerLog] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    const getAnswerInfo = async () => {
+        await fetch(`/api/answerlog/${props.q.id}/${currentUser.uid}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setAnswerLog(data)
+                setLoading(false)
+        })
+    }
 
     const answerFunction = async e => {
         e.preventDefault()
@@ -23,7 +37,24 @@ export default function QuestionReal(props) {
         if (answer === props.q.answer) {
             setCheckmark(true)
         }
+        const j = `{
+            "questionId": "${props.q.id}",
+            "respondentUsername": "${currentUser.displayName}",
+            "respondentUid": "${currentUser.uid}",
+            "respondentAnswer": "${answer}",
+            "correct": "${answer === props.q.answer}"
+        }`
+        console.log(j)
+        await fetch('/api/answer', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: j
+        }).then(response => console.log(response))
     }
+
+    useEffect(() => {
+        getAnswerInfo()
+    }, [])
 
     if (!props.enabled) {
         return (
@@ -73,6 +104,8 @@ export default function QuestionReal(props) {
                     </div>
                 </Card.Subtitle>
                 <Card.Subtitle className="mb-2 text-muted"><span style={{ color: themeText }}>{props.q.category} #{props.q.id}</span></Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted"><span style={{ color: themeText }}>{answerLog.length > 0 &&
+                    <div>Answered {answerLog[answerLog.length - 1].correct ? 'correctly' : 'incorrectly'} on {answerLog[answerLog.length - 1].date.substring(0, 10)}</div>}</span></Card.Subtitle>
                 <Card.Title style={{ fontSize: '20px' }}>{props.q.question}</Card.Title>
                 {(props.q.file && props.q.filetype === 'image') && <img style={{ maxWidth: '350px', marginTop: '10px' }} src={props.q.file} alt="user uploaded image" />}
                 {(props.q.file && props.q.filetype === 'audio') && <ReactAudioPlayer style={{ marginTop: '10px' }} src={props.q.file} controls />}
@@ -100,10 +133,17 @@ export default function QuestionReal(props) {
                 <div className="question-main">
                     {checkmark &&
                         <div>
-                            <Card.Title style={{ fontSize: '20px' }}>Correct</Card.Title>
-                            <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" /><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" /></svg>
+                            <Card.Title style={{ fontSize: '20px' }}>Correct!</Card.Title>
+                            <svg style={{ boxShadow: `inset 0px 0px 0px ${themeText}` }} className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle style={{ stroke: themeText }} className="checkmark__circle" cx="26" cy="26" r="25" fill="none" /><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" /></svg>
                             <p>{props.q.explanation ? 'Explanation: ' + props.q.explanation : 'no explanation available'}</p>       
-                        </div>
+                            <p>Question Log:</p>
+                            <p>Answered correctly just now</p>
+                            {answerLog.reverse().map((l, key) => {
+                                return <div>
+                                    Answered {l.correct ? 'correctly' : 'incorrectly'} on {l.date.substring(0, 10)}
+                                </div>
+                            })}          
+                         </div>
                     }
                     {!checkmark && 
                         <div>
@@ -111,9 +151,16 @@ export default function QuestionReal(props) {
                             <Card.Body>
                                 <p>Correct Answer: ({props.q.answer}) {answerText}</p>
                                 <p>{props.q.explanation ? 'Explanation: ' + props.q.explanation : 'no explanation available'}</p>
-                            </Card.Body>
+                                <p style={{ margin: '0px !important', padding: '0px !important'}}>Question Log:</p>
+                                <p>Answered incorrectly just now</p>
+                                {answerLog.reverse().map((l, key) => {
+                                    return <p id="question-p">
+                                        Answered {l.correct ? 'correctly' : 'incorrectly'} on {l.date.substring(0, 10)}
+                                    </p>
+                                })}
+                                </Card.Body>
                         </div>
-                    }
+                }
                 </div>
             }
         </Card>
--- functions/routes/answers.js
+++ functions/routes/answers.js
@@ -7,10 +7,23 @@ const pool = require("../db.js");
 
 module.exports = function (app) {
 
-    app.get("/api/questions", async (req, res) => {
+    app.post("/api/answer", async (req, res) => {
         try {
-            const allWorks = await pool.query("SELECT * FROM questions");
-            res.json(allWorks.rows);
+            const { questionId, respondentUsername, respondentUid, respondentAnswer, correct } = req.body;
+            const answerPost = await pool.query("INSERT INTO answers (questionid, respondentusername, respondentuid, date, respondentanswer, correct) VALUES ($1, $2, $3, CURRENT_DATE, $4, $5)",
+                [questionId, respondentUsername, respondentUid, respondentAnswer, correct]);
+            res.json(answerPost.rows);
+        } catch (err) {
+            console.error(err.message);
+        }
+    })
+
+    app.get("/api/answerlog/:id/:uid", async (req, res) => {
+        try {
+            const { id, uid } = req.params;
+            const answerLog = await pool.query("SELECT * FROM answers WHERE questionid = $1 AND respondentuid = $2",
+                [id, uid]);
+            res.json(answerLog.rows);
         } catch (err) {
             console.error(err.message);
         }
